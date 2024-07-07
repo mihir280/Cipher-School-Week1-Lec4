89ce60ee87578c49347fc8f661b758cf8f5020e3
Mihir Chandra <158182224+mihir280@users.noreply.github.com>
2024-07-07 20:13:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+  // return (
+   
+    function App() {
+      const name='CipherSchools'
+      return (
+        <div>
+          <h1 style={{color: "red",textAline: "center"}}>Hello from {name}</h1>
+        </div>
+      );
+    }
+  // );
+// }
+
+export default App;
